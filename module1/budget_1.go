031666ce556ac7b1cc1b4cbf1af62bf44b395e32
Arun Chopra <achopra405@gmail.com>
2021-10-04 08:27:17 -0400

    commit 2

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -3,10 +3,11 @@ package module1
 // Budget stores budget information
 type Budget struct {
 	Items []Item
+	Max   float32
 }
 
 // Item stores item information
 type Item struct {
 	Description string
-	Value       float32
+	Price       float32
 }
