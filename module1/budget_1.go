d728af1886d963d9816e32e46dbd7cdbc390a1b9
Arun Chopra <achopra405@gmail.com>
2021-10-04 06:34:20 -0400

    trial commit

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,12 @@
 package module1
 
 // Budget stores budget information
+type Budget struct {
+	Items []Item
+}
 
 // Item stores item information
+type Item struct {
+	Description string
+	Value       float32
+}
